67218e43bfcb917c290d352c1163cd9e55db1079
Nikita <61546432+N1kraut@users.noreply.github.com>
2021-01-21 01:43:49 +0300

    first

---
+++ home.php
@@ -0,0 +1,18 @@
+<style type="text/css">
+ @import url("style.css");
+</style>
+
+<div id = "LogPass">
+
+	<img src="img/anon.png">
+
+	<input type="text" class = "in" name = "Login" placeholder="Username" autocomplete="off">
+		
+	<input type="password" class = "in" name = "Password" placeholder="Password" autocomplete="off">
+
+	<input type="submit" name="Go" class = 'go' value="Log In" />
+
+</div>
+
+
+
